38343af79979232a9b8bb3930863e6b9452772b0
Ivana <ivana.gyro@gmail.com>
2023-01-07 13:31:31 +0800

    Add general methods for save and load objects

--- precalculation.py
+++ precalculation.py
@@ -9,14 +9,22 @@ dump_foler = root_folder / 'dumps'
 os.makedirs(dump_foler, exist_ok=True)
 
 
+def save_obj(obj, filename):
+    save(obj, str(dump_foler / filename))
+
+
 def save_current_ranges(l, depth, current_ranges):
     filename = f'current_ranges-l_{l}-depth_{depth}.sobj'
-    save(current_ranges, str(dump_foler / filename))
+    save_obj(current_ranges, filename)
 
 
-def load_current_ranges(l, depth):
-    filename = f'current_ranges-l_{l}-depth_{depth}.sobj'
+def load_obj(filename):
     try:
         return load(str(dump_foler / filename))
     except FileNotFoundError:
         return None
+
+
+def load_current_ranges(l, depth):
+    filename = f'current_ranges-l_{l}-depth_{depth}.sobj'
+    return load_obj(filename)
